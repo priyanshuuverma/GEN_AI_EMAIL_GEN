da2548754e76613eca7c0547daab0257b15f7983
Priyanshu Verma <officialpriyanshu2003@gmail.com>
2025-03-26 13:35:38 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -18,4 +18,4 @@ class Portfolio:
                                     ids=[str(uuid.uuid4())])
 
     def query_links(self, skills):
-        return self.collection.query(query_texts=skills, n_results=2).get('metadatas', [])
+        return self.collection.query(query_texts=skills, n_results=5).get('metadatas', [])
